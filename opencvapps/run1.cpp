b52878040002a8265a84cd0329963e53225ea5cd
Joohwi Lee <telnet2@gmail.com>
2013-01-29 15:27:39 -0500

    trying opencv

---
+++ opencvapps/run1.cpp
@@ -0,0 +1,13 @@
+/**
+ * opencv app
+ *
+ */
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cv::namedWindow("First", CV_WINDOW_AUTOSIZE);
+}
