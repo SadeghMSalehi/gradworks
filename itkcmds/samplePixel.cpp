90ae6e1b65e964b8f365afd330cbca1f84c2258e
Joohwi Lee <telnet2@gmail.com>
2013-02-05 00:39:01 -0500

    adding sample pixel tool

---
+++ itkcmds/samplePixel.cpp
@@ -0,0 +1,43 @@
+#include "itkImageIO.h"
+#include "itkImage.h"
+#include "iostream"
+
+typedef itk::Image<double,3> Image;
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        cout << "usage: " << argv[0] << " input.lst image.nrrd output.txt" << endl;
+        return 0;
+    }
+
+    itkcmds::itkImageIO<Image> io;
+    Image::Pointer img = io.ReadImageT(argv[2]);
+
+    ifstream in(argv[1]);
+    if (!in.is_open()) {
+        return 0;
+    }
+
+    ofstream out(argv[3]);
+    while (in.good()) {
+        double x, y, z;
+        in >> x >> y >> z;
+
+        // assume physical point
+        Image::PointType inPoint;
+        inPoint[0] = x;
+        inPoint[1] = y;
+        inPoint[2] = z;
+
+        Image::IndexType outIdx;
+
+        img->TransformPhysicalPointToIndex(inPoint, outIdx);
+        double pixel = img->GetPixel(outIdx);
+        out << pixel << endl;
+    }
+
+    out.close();
+    in.close();
+}
