f9b180ab6bf579d5bdae4d2d7d44d50854477c84
Joohwi Lee <telnet2@gmail.com>
2013-04-22 23:50:23 -0400

    texture opengl

--- Graphics/GLTest/QGlyphWidget.cpp
+++ Graphics/GLTest/QGlyphWidget.cpp
@@ -20,18 +20,24 @@ void QGlyphWidget::initializeGL() {
     glClearColor(0.2f, 0.2f, 0.2f, 0);
     glClearDepth(1.0f);
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);                        // Enable Texture Mapping ( NEW )
+    glDisable(GL_CULL_FACE);
     glDepthFunc(GL_LEQUAL);
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-    glEnable(GL_BLEND);
+
     glEnable(GL_POLYGON_SMOOTH);
     glEnable(GL_LINE_SMOOTH);
     glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
     glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+//        glEnable(GL_BLEND);
+    //    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glShadeModel(GL_SMOOTH);
 }
 
 void QGlyphWidget::resizeGL(int w, int h) {
+    loadTextures();
+    
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -41,36 +47,64 @@ void QGlyphWidget::resizeGL(int w, int h) {
     glLoadIdentity();
 }
 
+void QGlyphWidget::loadTextures() {
+    QImage image;
+    image.load("/Users/joohwi/Downloads/Clown-Fish-05.jpg");
+    QImage glImage = convertToGLFormat(image);
+    
+    glGenTextures(1, _texture);
+    for (int i = 0; i < 1; i++) {
+        glBindTexture(GL_TEXTURE_2D, _texture[i]);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, glImage.width(), glImage.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, glImage.scanLine(0));
+    }
+}
+
 void QGlyphWidget::paintGL() {
-    static GLfloat rTri = 0.0f;
-    static GLfloat rQuad = 0.0f;
-
+    static GLfloat rx = 0.0f, ry = 0.0f, rz = 0.0f;
+ 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glLoadIdentity();                       // Reset The View
-    glTranslatef(-1.5f,0.0f,-6.0f);                 // Move Left 1.5 Units And Into The Screen 6.0
-    glRotatef(rTri, 0, 0.f, 1.0f);
-    glBegin(GL_TRIANGLES);                      // Drawing Using Triangles
-    glColor3f( 1.0f, 0.0f, 0.0f);          // Set The Color To Red
-    glVertex3f( 0.0f, 1.0f, 0.0f);              // Top
-    glColor3f( 0.0f, 1.0f, 0.0f);          // Set The Color To Green
-    glVertex3f(-1.0f,-1.0f, 0.0f);              // Bottom Left
-    glColor3f(0.0f,0.0f,1.0f);
-    glVertex3f( 1.0f,-1.0f, 0.0f);              // Bottom Right
-    glEnd();                            // Finished Drawing The Triangle
-
+    
+    float cubecoord[6][12] = {
+        {-1,-1,1, 1,-1,1, 1,1,1, -1,1,1},
+        {-1,-1,-1, 1,-1,-1, 1,1,-1, -1,1,-1},
+        {-1,1,-1, -1,1,1, 1,1,1, 1,1,-1},
+        {-1,-1,-1, 1,-1,-1, 1,-1,1, -1,-1,1},
+        {1,-1,-1, 1,1,-1, 1,1,1, 1,-1,1},
+        {-1,-1,-1, -1,-1,1, -1,1,1, -1,1,-1},
+    };
+    
+    float cubetexcoord[6][8] = {
+        { 0,0, 1,0, 1,1, 0,1 },
+        { 1,0, 1,1, 0,1, 0,0 },
+        { 0,1, 0,0, 1,0, 1,1 },
+        { 1,1, 0,1, 0,0, 1,0 },
+        { 1,1, 0,1, 0,0, 1,0 },
+        { 1,0, 1,1, 0,1, 0,0 }
+    };
+    
     glLoadIdentity();
-    glColor3f(1.0f,1.0f,1.0f);
-    glTranslatef(3.0f,0.0f,0.0f);                   // Move Right 3 Units
-//    glRotatef(rQuad, 0, 1.f, 0);
-    glBegin(GL_QUADS);                      // Draw A Quad
-    glVertex3f(-1.0f, 1.0f, 0.0f);              // Top Left
-    glVertex3f( 1.0f, 1.0f, 0.0f);              // Top Right
-    glVertex3f( 1.0f,-1.0f, 0.0f);              // Bottom Right
-    glVertex3f(-1.0f,-1.0f, 0.0f);              // Bottom Left
-    glEnd();                            // Done Drawing The Quad
+    glTranslatef(0.0f,0.0f,-5.0f);                      // Move Into The Screen 5 Units
 
-    rTri += 0.2f;
+    glRotatef(rx,1,0,0);
+    glRotatef(ry,0,1,0);
+    glRotatef(rz,0,0,1);
+    
+    glBindTexture(GL_TEXTURE_2D, _texture[0]);
+    glColor3f(1, 1, 1);
+    glBegin(GL_QUADS);
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 4; j++) {
+            glTexCoord2fv(&cubetexcoord[i][j*2]);
+            glVertex3fv(&cubecoord[i][j*3]);
+        }
+    }
+    glEnd();
+
+    rx += 0.2f;
+    ry += 0.3f;
+    rz += 0.4f;
 }
 
 void QGlyphWidget::keyReleaseEvent(QKeyEvent *key) {
--- Graphics/GLTest/QGlyphWidget.h
+++ Graphics/GLTest/QGlyphWidget.h
@@ -13,6 +13,18 @@
 #include <QGLWidget>
 #include <QKeyEvent>
 
+struct TexQuad {
+    float xcoords[16];
+    float tcoords[8];
+    
+    TexQuad() {
+        memset(xcoords, 0, sizeof(xcoords));
+        memset(tcoords, 0, sizeof(tcoords));
+    }
+};
+
+typedef std::vector<TexQuad> TexQuadArray;
+
 class QGlyphWidget: public QGLWidget {
     Q_OBJECT
 public:
@@ -23,8 +35,12 @@ protected:
     void initializeGL();
     void resizeGL(int w, int h);
     void paintGL();
-
     void keyReleaseEvent(QKeyEvent*);
+    
+    void loadTextures();
+    
+private:
+    GLuint _texture[1];
 };
 
 #endif /* defined(__gltest__QGlyphWidget__) */
