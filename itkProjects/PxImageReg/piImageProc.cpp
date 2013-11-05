c6853f35b3ff2a6bd8f827d1c47b42a6faf7460c
Joohwi Lee <telnet2@gmail.com>
2013-11-05 11:36:41 -0500

    ensemble term balancing with force

--- itkProjects/PxImageReg/piImageProc.cpp
+++ itkProjects/PxImageReg/piImageProc.cpp
@@ -10,6 +10,7 @@
 #include "piImageIO.h"
 
 #include <itkSignedDanielssonDistanceMapImageFilter.h>
+#include <itkExtractImageFilter.h>
 
 using namespace std;
 
@@ -110,5 +111,32 @@ namespace pi {
         tracker->SetDirection(direction);
 
         return tracker;
+    }
+    
+    RealImage2Vector SliceVolume(RealImage3::Pointer volume, int dim) {
+        RealImage2Vector slices;
+        RealImage3::RegionType region = volume->GetBufferedRegion();
+        RealImage3::SizeType sz = region.GetSize();
+        const RealImage3::IndexType idx = region.GetIndex();
+        
+        const int nSlices = sz[dim];
+        slices.resize(nSlices);
+        region.SetSize(dim, 0);
+        
+        typedef itk::ExtractImageFilter<RealImage3, RealImage2> FilterType;
+        
+        for (int i = idx[dim]; i < idx[dim] + nSlices; i++) {
+            region.SetIndex(dim, i);
+            
+            FilterType::Pointer filter = FilterType::New();
+            filter->SetInput(volume);
+            filter->SetExtractionRegion(region);
+            filter->SetDirectionCollapseToGuess();
+            filter->Update();
+            slices[i] = filter->GetOutput();
+            slices[i]->DisconnectPipeline();
+        }
+        
+        return slices;
     }
 }
