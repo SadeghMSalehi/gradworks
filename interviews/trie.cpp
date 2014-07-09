c7396d5c224aba467cc7102a2ecc033878404c93
Joowhi <telnet2@gmail.com>
2014-07-08 23:02:27 -0400

    Adding new tools

--- interviews/trie.cpp
+++ interviews/trie.cpp
@@ -7,3 +7,20 @@
 //
 
 #include "trie.h"
+#include <iostream>
+
+using namespace std;
+
+void trie_test() {
+    Trie t;
+    t.push("amy");
+    t.push("ann");
+    t.push("emma");
+    t.push("rob");
+    t.push("roger");
+
+    
+    if (t.has("amy")) {
+        cout << t["amy"] << endl;
+    }
+}
--- interviews/trie.h
+++ interviews/trie.h
@@ -10,5 +10,60 @@
 #define __interviews__trie__
 
 #include <iostream>
+#include <string>
+
+
+class TrieNode {
+public:
+    // assume that only lower cases characters are stored
+    std::string prefix;
+    TrieNode* alphabets[27];
+    TrieNode* child;
+};
+
+template <class V>
+class TrieValue {
+public:
+private:
+    V value;
+};
+
+
+template <class V>
+class Trie {
+public:
+    void add(std::string k, V v);
+    bool remove(std::string k);
+    bool has(std::string k);
+    V operator[](std::string key);
+};
+
+class NoKeyException {
+    
+};
+
+
+template <class V>
+void Trie<V>::add(std::string k, V v) {
+    
+}
+
+template <class V>
+bool Trie<V>::remove(std::string k) {
+    if (!has(k)) {
+        return false;
+    }
+}
+
+template <class V>
+V Trie<V>::operator[](std::string k) {
+    if (!has(k)) {
+        throw NoKeyException();
+    }
+}
+
+
+
+void trie_test();
 
 #endif /* defined(__interviews__trie__) */
